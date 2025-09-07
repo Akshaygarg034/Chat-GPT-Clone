573083e10d912eaaa7f31364af6003dccaa43640
Akshay Garg <gargakshay034@gmail.com>
2025-09-07 22:38:57 +0530

    typescript version issue in build fix

--- models/Chat.ts
+++ models/Chat.ts
@@ -1,10 +1,9 @@
-// models/Chat.ts
-import { Schema, model, models } from 'mongoose'
+import { Schema, model, models } from "mongoose";
 
 const ChatSchema = new Schema({
-  chatId:    { type: String, required: true, unique: true },
+  chatId: { type: String, required: true },
   userEmail: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-})
+});
 
-export default models.Chat || model('Chat', ChatSchema)
+export default models.Chat || model("Chat", ChatSchema);
