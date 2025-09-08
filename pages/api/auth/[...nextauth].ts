d6680735f51d578f1bf80732032c3ee73c3dd181
Akshay Garg <gargakshay034@gmail.com>
2025-09-08 23:19:54 +0530

    fixed logout issue

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,6 +7,13 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
+      authorization: {
+        params: {
+          prompt: "select_account", // always show account selector
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
     // ...add more providers here
   ],
